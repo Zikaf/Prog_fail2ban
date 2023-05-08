a5bfe8ae1eb704f4ca0ec4187d3f5858e7737c77
Zikaf <131281714+Zikaf@users.noreply.github.com>
2023-05-08 15:17:25 +0300

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -2,23 +2,9 @@
 
 # Установка
 apt update
+apt install rsyslog -y
 apt install fail2ban -y
 
-# Создание директории
-mkdir -p /var/log_fail2ban/
-touch /var/log_fail2ban/vsftpd.log
-touch /var/log_fail2ban/postgresql.log
-touch /var/log_fail2ban/mysqld.log
-touch /var/log_fail2ban/mongod.log
-touch /var/log_fail2ban/samba.log
-touch /var/log_fail2ban/apache2.log
-touch /var/log_fail2ban/nginx.log
-touch /var/log_fail2ban/openvpn.log
-touch /var/log/auth.log
-
-# Выдача прав
-chmod 644 /var/log/auth.log
-chown root:adm /var/log/auth.log
 # Настройка конфига
 cp jail.local /etc/fail2ban/jail.local
 
