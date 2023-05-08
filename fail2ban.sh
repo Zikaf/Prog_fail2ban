839d54d31a84895857d1c2da98076ef155cb7d10
Zikaf <131281714+Zikaf@users.noreply.github.com>
2023-05-08 12:32:49 +0300

    Create fail2ban.sh

---
+++ fail2ban.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Установка
+apt install fail2ban -y
+
+# Создание директории
+mkdir -p /var/log_fail2ban/
+touch /var/log_fail2ban/vsftpd.log
+touch /var/log_fail2ban/postgresql.log
+touch /var/log_fail2ban/mysqld.log
+touch /var/log_fail2ban/mongod.log
+touch /var/log_fail2ban/samba.log
+touch /var/log_fail2ban/apache2.log
+touch /var/log_fail2ban/nginx.log
+touch /var/log_fail2ban/openvpn.log
+touch /var/log/auth.log
+
+# Выдача прав
+chmod 644 /var/log/auth.log
+chown root:adm /var/log/auth.log
+# Настройка конфига
+mv jail.local /etc/fail2ban/jail.local
+
+# Запуск и проверка fail2ban
+systemctl start fail2ban
+systemctl status fail2ban
+fail2ban-client status sshd
